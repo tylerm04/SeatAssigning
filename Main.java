5f7840034523297f4b89c080ed25a34e1462ed87
Tyler Mcdowell <tyler.mcdowell@patelhs.org>
2020-11-04 18:37:30 +0000

    Ive set up the arrays

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+import java.util.Scanner;
+
+
+class Main 
+{
+  public static void main(String[] args) 
+  {
+
+
+   int[] seatNum = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+   for (int i = 0; i < seatNum.length; i++)
+
+   {
+     System.out.println(  seatNum[i] );
+   }
+   {
+      String[] names = new String[15];
+      
+      for (int i = 0; i < names.length; i++)
+
+
+
+   }
+
   }
 }
