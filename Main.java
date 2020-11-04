b06884a22d0d21a9ccacb20b38df771867195658
Tyler Mcdowell <tyler.mcdowell@patelhs.org>
2020-11-04 18:58:37 +0000

    i fixed errors and added scanner

--- Main.java
+++ Main.java
@@ -1,25 +1,19 @@
 import java.util.Scanner;
 
-
 class Main 
 {
   public static void main(String[] args) 
   {
-
-
    int[] seatNum = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    String[] names = new String[15];
 
    for (int i = 0; i < seatNum.length; i++)
-
    {
-     System.out.println(  seatNum[i] );
-   }
-   {
-      String[] names = new String[15];
-      
-      for (int i = 0; i < names.length; i++)     
+    Scanner scan = new Scanner( System.in );
 
+    System.out.println("What is your name? " );
 
+    String ans = scan.nextLine();
 
    }
 
